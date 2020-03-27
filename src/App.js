59d8be1735b73ccd136a6cd48dae96480d876999
Ivan S <ing.ivaneduardosanchez@hotmail.com>
2020-03-27 11:27:48 -0500

    Adding header container and links to home and shop pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopData from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
+import Header from "./components/header/header.component";
 
 const HatsPage = () => (
   <div>
@@ -13,6 +14,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopData} />
